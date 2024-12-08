b09ac89fa54582379fd3a5c5dfa7b2b9af33baa8
prakashtaz0091 <code.ing2468@gmail.com>
2024-12-08 22:20:27 +0545

    deploy changes

--- app/forms.py
+++ app/forms.py
@@ -72,11 +72,12 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         # fields = '__all__'
-        fields = ['title', 'body']
+        fields = ['title', 'body', 'tags']
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags':forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
 
 
