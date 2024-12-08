adb5b4a48d54433c15ff2ec215accedb4ca0f5c4
prakashtaz0091 <code.ing2468@gmail.com>
2024-12-08 22:08:46 +0545

    deploy changes

--- project/settings.py
+++ project/settings.py
@@ -23,6 +23,8 @@ import dj_database_url
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
