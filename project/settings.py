ba9aba6abc741ebab347dd4414bd1cbd253c7929
prakashtaz0091 <code.ing2468@gmail.com>
2024-12-08 22:42:09 +0545

    deploy changes

--- project/settings.py
+++ project/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+import environ
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
